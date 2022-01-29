0ab93ebd270b53539aca62812bb52b64ca50ed90
Asfaw Nahom <s2200108@BC-C02GR0BCQ6LR-MacBook-Air.local>
2022-01-29 18:24:03 +0200

    some change

--- JS/4_Task/ex_6.js
+++ JS/4_Task/ex_6.js
@@ -1,7 +1,7 @@
 let arr = [];
 let sum = 0;
 for (let i = 0; i < 5; i++) {
-  let num = prompt("Enter 25 numbers");
+  let num = prompt("Enter numbers");
   arr.push(Number(num));
 }
 let ave = arr.reduce((pv, cv) => pv + cv / 2);
