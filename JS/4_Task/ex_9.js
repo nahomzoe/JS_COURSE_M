4030db303bb0dbae6d1a6e71b5b230c690c75101
Asfaw Nahom <s2200108@BC-C02GR0BCQ6LR-MacBook-Air.local>
2022-01-30 12:54:16 +0200

    ex_9 update

--- JS/4_Task/ex_9.js
+++ JS/4_Task/ex_9.js
@@ -3,9 +3,36 @@ let arr = [];
 for (let i = 0; i < 10; i++) {
   let num = Number(prompt("Enter 10 numbers"));
   arr.push(num);
-  let arrMin = [Math.min(...arr)];
-  let arrWithoutMin = arr.slice(arrMin);
-  let minOfarrWithoutMin = [Math.min(...arrWithoutMin)];
-  let arrSecondbestMin = arrMin.concat(minOfarrWithoutMin);
-  console.log(arrSecondbestMin);
+  if (arr === true) {
+    let arrMin = [Math.min(...arr)];
+    let arrWithoutMin = arr.slice(arrMin);
+    let minOfarrWithoutMin = [Math.min(...arrWithoutMin)];
+    let arrSecondbestMin = arrMin.concat(minOfarrWithoutMin);
+    console.log(arrSecondbestMin);
+  }
 }
+
+/*let arr = [];
+
+for (let i = 0; i < 10; i++) {
+  let num = Number(prompt("Enter 10 numbers"));
+  arr.push(num);
+}
+for (let i = 0; i < 10; i++) {
+  if (arr[i] < arrMin) {
+    let arrMin = arr[0];
+    arrMin = arr[i];
+    console.log(arrMin);
+  }
+  console.log(arrMin);
+}*/
+
+// the next one works perfectly
+/*let arr = [];
+
+for (let i = 0; i < 10; i++) {
+  let num = Number(prompt("Enter 10 numbers"));
+  arr.push(num);
+  let arrMin = arr.sort((a, b) => a - b);
+  console.log(`${arrMin[0]} ${arrMin[1]}`);
+}*/
