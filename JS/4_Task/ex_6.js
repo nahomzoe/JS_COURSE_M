83d97784ffa9795ecc80cd95efe794fcb9a09927
Asfaw Nahom <s2200108@BC-C02GR0BCQ6LR-MacBook-Air.local>
2022-01-30 08:13:30 +0200

    ex_6 more correction

--- JS/4_Task/ex_6.js
+++ JS/4_Task/ex_6.js
@@ -1,8 +1,8 @@
 let arr = [];
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 25; i++) {
   let num = prompt("Enter numbers");
   arr.push(Number(num));
 }
 let sumOfarr = arr.reduce((pv, cv) => pv + cv);
 
-console.log(sumOfarr / 5);
+console.log(sumOfarr / 25);
