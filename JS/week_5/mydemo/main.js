d3a1b0dd954d8edd4156cad1e99962b13aed4161
Asfaw Nahom <s2200108@edu.bc.fi>
2022-03-04 09:26:24 +0200

    cucumber

--- JS/week_5/mydemo/main.js
+++ JS/week_5/mydemo/main.js
@@ -1,14 +1,16 @@
-let header = document.querySelector("header");
-let button = document.querySelector("#backToTop");
-let mobButton = document.querySelector("#mobileButton");
-let nav = document.querySelector("nav");
+const header = document.querySelector("header");
+const button = document.querySelector("#backToTop");
+const mobButton = document.querySelector("#mobileButton");
+const nav = document.querySelector("nav");
+
+let links = document.querySelectorAll("nav ul li a");
 
 window.onscroll = function () {
   scrollFunction();
 };
 
 const scrollFunction = () => {
-  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
     header.classList.add("bg");
     button.style.display = "block";
   } else {
@@ -23,10 +25,15 @@ const getToTop = () => {
 };
 
 const mobMenu = () => {
-  if (nav.classList.contains("responsive")) {
-    nav.classList.remove("responsive");
+  for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", mobMenu);
+  }
+  if (nav.classList.contains("tool-bar")) {
+    nav.classList.remove("tool-bar");
+    document.body.style.overflow = "";
   } else {
-    nav.classList.add("responsive");
+    nav.classList.add("tool-bar");
+    document.body.style.overflow = "hidden";
   }
 };
 
--- Portofolio metropolia/main.js
+++
@@ -1,8 +0,0 @@
-const buttonTop = document.querySelector("#returnToTop");
-
-const getToTop = () => {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-};
-
-buttonTop.addEventListener("click", getToTop);
